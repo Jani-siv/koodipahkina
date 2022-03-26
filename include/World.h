03f2fa0740b8c5e90322009a6bf40d8d0acab0d5
Jani-siv <info@kengityspaja.fi>
2022-03-26 15:25:12 +0200

    function to create multiple edges for container

--- include/World.h
+++ include/World.h
@@ -11,6 +11,7 @@ class World : public Edge, Node {
         ~World();
         void createNodes();
     private:
+        void createEdgesForContainer(std::list<int> edge);
         std::map<Edge,std::map<Node,Node>>connection;
         std::map<int,Node> nodes;
         std::map<int,Edge> edges;
--- src/World.cpp
+++ src/World.cpp
@@ -11,34 +11,34 @@ World::~World()
 void World::createNodes()
 {
     //read file to get node name, x, y
-    Node nod0, nod1;
+    Node nod0, nod1, nod2;
     nod0.addNode(0, 0, 0);
-    std::cout<<"node name: "<<nod0.getName()<<std::endl;
     nod1.addNode(1, 1, 3);
+    nod2.addNode(2, 2, 2);
     //read node edges
-    std::list<int> rEdge;
-    rEdge.push_back(11);
+    std::list<int> rEdge0, rEdge1, rEdge2;
+    rEdge0.push_back(11);
+    rEdge0.push_back(13);
+    
+    rEdge1.push_back(11);
+    rEdge1.push_back(12);
+
+    rEdge2.push_back(12);
+    rEdge2.push_back(13);
+
     //add edges to node
-    nod0.addEdges(rEdge);
-    nod1.addEdges(rEdge);
+    nod0.addEdges(rEdge0);
+    nod1.addEdges(rEdge1);
+    nod2.addEdges(rEdge2);
     //create edge
     //avoid double edges
-    for ( auto i = rEdge.begin(); i != rEdge.end(); i++)
-    {
-        std::cout<<"Edge name: "<<i.operator*()<<std::endl;
-        auto it = this->edges.find(i.operator*());
-        if (it == this->edges.end())
-        {
-            Edge e;
-            e.createEdge(i.operator*());
-            this->edges[i.operator*()] = e;
-            std::cout<<"creating edge"<<std::endl;
-        }
-    }
-
+    this->createEdgesForContainer(rEdge0);
+    this->createEdgesForContainer(rEdge1);
+    this->createEdgesForContainer(rEdge2);
     //add nodes in container
     this->nodes[0] = nod0;
     this->nodes[1] = nod1;
+    this->nodes[2] = nod2;
 
     //connect
     for (auto i = this->nodes.begin(); i != this->nodes.end(); i++)
@@ -67,4 +67,18 @@ void World::createNodes()
         std::cout<<"edge name: "<<i->first<<" Point A: "<<i->second.pointA<<" Point B: "<<i->second.pointB<<std::endl;
     }
 
+}
+
+void World::createEdgesForContainer(std::list<int> edge)
+{
+   for (auto i = edge.begin(); i != edge.end(); i++)
+   {
+       auto it = this->edges.find(i.operator*());
+       if (it == this->edges.end())
+       {
+           Edge e;
+           e.createEdge(i.operator*());
+           this->edges[i.operator*()] = e;
+       }
+   }
 }
